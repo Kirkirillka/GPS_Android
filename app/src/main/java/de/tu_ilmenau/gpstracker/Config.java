225ac24573b8bd467b76bb7514059884ee90e12e
Khakov Rustam <khakov.rustam@gmail.com>
2020-02-01 19:11:54 +0300

    Fix connection provider

--- app/src/main/java/de/tu_ilmenau/gpstracker/Config.java
+++ app/src/main/java/de/tu_ilmenau/gpstracker/Config.java
@@ -3,5 +3,5 @@ package de.tu_ilmenau.gpstracker;
 import android.location.LocationManager;
 
 public class Config {
-    public static final String LOC_MANAGER = LocationManager.NETWORK_PROVIDER;
+    public static final String LOC_MANAGER = LocationManager.GPS_PROVIDER;
 }
