e2476149ecdba283d163d85e5fcd0ce8f7ea33be
Khakov Rustam <khakov.rustam@gmail.com>
2020-02-05 17:45:09 +0300

    Change config

--- app/src/main/java/de/tu_ilmenau/gpstracker/Config.java
+++ app/src/main/java/de/tu_ilmenau/gpstracker/Config.java
@@ -4,6 +4,6 @@ import android.location.LocationManager;
 
 public class Config {
     public static final String LOC_MANAGER = LocationManager.GPS_PROVIDER;
-    public final static String DOWNLOAD_URL = "http://ipv4.ikoula.testdebit.info/1M.iso";
-    public final static String UPLOAD_URL = "http://ipv4.ikoula.testdebit.info/";
+    public final static String DOWNLOAD_URL = "ftp://speedtest:speedtest@192.168.50.4/1MB.zip";
+    public final static String UPLOAD_URL = "ftp://speedtest:speedtest@192.168.50.4/";
 }
