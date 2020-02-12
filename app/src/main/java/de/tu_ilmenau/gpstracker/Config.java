165023c2259cb32e8ae07b5d5eaad24d6789138c
Kir <kirillkukarcev@gmail.com>
2020-02-12 03:10:51 +0100

    Use AP's hosted FTP server.

--- app/src/main/java/de/tu_ilmenau/gpstracker/Config.java
+++ app/src/main/java/de/tu_ilmenau/gpstracker/Config.java
@@ -4,6 +4,6 @@ import android.location.LocationManager;
 
 public class Config {
     public static String LOC_MANAGER = LocationManager.GPS_PROVIDER;
-    public final static String DOWNLOAD_URL = "ftp://speedtest:speedtest@192.168.50.4/1MB.zip";
-    public final static String UPLOAD_URL = "ftp://speedtest:speedtest@192.168.50.4/upload";
+    public final static String DOWNLOAD_URL = "ftp://speedtest:speedtest@192.168.10.1/1MB.zip";
+    public final static String UPLOAD_URL = "ftp://speedtest:speedtest@192.168.10.1/upload";
 }
