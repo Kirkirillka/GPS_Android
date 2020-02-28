126ac6a8dbb325be57191a4d0396665bdf9d4e5d
Kir <kirillkukarcev@gmail.com>
2020-02-28 20:42:23 +0100

    Add new fields to avoid usage of temporary class to store result

--- app/src/main/java/de/tu_ilmenau/gpstracker/model/SpeedTestTotalResult.java
+++ app/src/main/java/de/tu_ilmenau/gpstracker/model/SpeedTestTotalResult.java
@@ -4,16 +4,39 @@ public class SpeedTestTotalResult {
     private double upSpeed;
     private double downSpeed;
 
+    private boolean upSpeedReady;
+    private boolean downSpeedReady;
+
     public double getUpSpeed() {
-        return upSpeed;
+        return Math.floor(upSpeed) ;
     }
 
     public void setUpSpeed(double upSpeed) {
         this.upSpeed = upSpeed;
     }
 
+    public boolean isFinish() {
+        return upSpeedReady | downSpeedReady;
+    }
+
+    public void setUpSpeedReady(boolean finish) {
+        this.upSpeedReady = finish;
+    }
+
+    public boolean isUplinkReady(){
+        return upSpeedReady;
+    }
+
+    public void setDownSpeedReady(boolean finish) {
+        this.downSpeedReady = finish;
+    }
+
+    public boolean isDownlinkReady(){
+        return downSpeedReady;
+    }
+
     public double getDownSpeed() {
-        return downSpeed;
+        return Math.floor(downSpeed);
     }
 
     public void setDownSpeed(double downSpeed) {
